0cf38873ff889d1c89654edd255f05af3a8dc03f
AbigailMezaC <mezacruzabigail@gmail.com>
2022-10-13 18:25:15 -0500

    CRUD

--- database/db.js
+++ database/db.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql');
+const mysql2 = require('mysql2');
 
-const conexion = mysql.createConnection({
+const conexion = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'n0m3l0123',
+    password: 'n0m3l0',
     database: 'crudnode'
 });
 
 conexion.connect((error)=>{
     if(error){
-        console.error('El error de conexión es:' + error)
+        console.error('El error de conexión es: ' + error)
         return
     }
     console.log('¡Conectado con la BD!')
