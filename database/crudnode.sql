fc12e09182d1e18f83e65e3773bfd302c0895e3f
AbigailMezaC <mezacruzabigail@gmail.com>
2022-10-13 12:41:00 -0500

    CRUD

---
+++ database/crudnode.sql
@@ -0,0 +1,4 @@
+create database crudnode;
+use crudnode;
+create table canciones (id int auto_increment, nom varchar(50), aut varchar(50), gen varchar(50), primary key (id));
+select * from canciones;
